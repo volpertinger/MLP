dc140de79ea5fcb0679b5d9dca97b25031321339
Nikolay <merzlovnik@mail.ru>
2023-03-22 23:33:58 +0300

    Log level

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import os
+import settings as s
 from dataset_utils import DataSet
 from processing_utils import start_test_input
 
 if __name__ == '__main__':
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = s.TF_CPP_MIN_LOG_LEVEL
     dataset = DataSet()
     dataset.train()
     start_test_input(dataset)
