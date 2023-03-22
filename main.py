d58f926befac459ad19f367a4a79ba217714dba4
Nikolay <merzlovnik@mail.ru>
2023-03-22 20:36:33 +0300

    Train

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dataset_utils import DataSet
-
 from processing_utils import start_test_input
 
 if __name__ == '__main__':
     dataset = DataSet()
+    dataset.train_model()
     start_test_input(dataset)
