b29a3918831b634299163684682b91af96fff198
Nikolay <merzlovnik@mail.ru>
2023-03-22 20:36:15 +0300

    Constants

--- settings.py
+++ settings.py
@@ -1,2 +1,8 @@
 DATASET = "horses_or_humans"
 BATCH_SIZE = 32
+LAYERS_NUMBER = 3
+NEURON_NUMBER = 4
+ACTIVATION = "elu"
+LOSS_FUNCTION = "adam"
+MAX_EPOCHS = 1
+SAVE_FILENAME = "model_saves/horses_or_humans"
